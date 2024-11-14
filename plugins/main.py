8a3472383ac26e502c26652190bc1a5e9bc18ebd
KichuTG <kichumon333@outlook.com>
2024-11-14 11:00:43 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -12,7 +12,7 @@ async def start_command(client, message):
 
     welcome_message = (
         "**👋 Welcome to Response By Ai!**\n\n"
-        "🤖 I'm your personal AI assistant, crafted with love by ⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ.\n\n"
+        "🤖 I'm your personal AI assistant, crafted with love by ⏤͟͟͞͞ 🇮🇳 @Kichuxd.\n\n"
         "✨ **Here’s what I can do for you:**\n"
         "Just click the buttons below to get started on this exciting journey! 🚀"
     )
@@ -20,8 +20,8 @@ async def start_command(client, message):
     keyboard = InlineKeyboardMarkup([
         [InlineKeyboardButton("🌟 ʜᴇʟᴘ", callback_data="help"),
          InlineKeyboardButton("ℹ️ ᴀʙᴏᴜᴛ", callback_data="about")],
-        [InlineKeyboardButton("📢 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/C0DE_SEARCH"),
-         InlineKeyboardButton("🛠️ sᴜᴘᴘᴏʀᴛ", url="https://t.me/AsuraaSupports")]
+        [InlineKeyboardButton("📢 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/AM_FILMS"),
+         InlineKeyboardButton("🛠️ sᴜᴘᴘᴏʀᴛ", url="https://t.me/Dramaship")]
     ])
 
     await client.send_photo(chat_id=message.chat.id, photo="https://envs.sh/p_g.jpg", caption=welcome_message, reply_markup=keyboard)
@@ -48,8 +48,8 @@ async def handle_button_click(client, callback_query):
         keyboard = InlineKeyboardMarkup([
             [InlineKeyboardButton("🌟 ʜᴇʟᴘ", callback_data="help"),
              InlineKeyboardButton("ℹ️ ᴀʙᴏᴜᴛ", callback_data="about")],
-            [InlineKeyboardButton("📢 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/C0DE_SEARCH"),
-             InlineKeyboardButton("🛠️ sᴜᴘᴘᴏʀᴛ", url="https://t.me/AsuraaSupports")]
+            [InlineKeyboardButton("📢 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/AM_FILMS"),
+             InlineKeyboardButton("🛠️ sᴜᴘᴘᴏʀᴛ", url="https://t.me/Dramaship")]
         ])
 
         await edit_message(client, callback_query, welcome_message, keyboard)
@@ -57,7 +57,7 @@ async def handle_button_click(client, callback_query):
     elif callback_query.data == "chatwithai":
         chat_keyboard = InlineKeyboardMarkup([
             [InlineKeyboardButton("🔙 ʙᴀᴄᴋ", callback_data="help"),
-             InlineKeyboardButton("🛠️ sᴜᴘᴘᴏʀᴛ", url="https://t.me/AsuraaSupports")]
+             InlineKeyboardButton("🛠️ sᴜᴘᴘᴏʀᴛ", url="https://t.me/Dramaship")]
         ])
         chat_message = (
             "**💬 Let’s Dive into a Conversation with AI!**\n\n ✨**Ready to explore?** Just use the commands below to unlock endless possibilities! ☀️\n\n✔️ **Got a question?** \n Send your question using **/ask**, and get valuable answers from ResponseByAi! 💡\n\nJoin the conversation and see what wonders await!"
@@ -67,14 +67,14 @@ async def handle_button_click(client, callback_query):
     elif callback_query.data == "image":
         image_keyboard = InlineKeyboardMarkup([
             [InlineKeyboardButton("🔙 ʙᴀᴄᴋ", callback_data="help"),
-             InlineKeyboardButton("🛠️ sᴜᴘᴘᴏʀᴛ", url="https://t.me/AsuraaSupports")]
+             InlineKeyboardButton("🛠️ sᴜᴘᴘᴏʀᴛ", url="https://t.me/Dramaship")]
         ])
         await edit_message(client, callback_query, "**🖼️ Your Creative Journey Starts Here!**\n\n\n**🎨 Unleash Your Creativity!** \n Type **/draw** followed by your vision, like “A futuristic city at sunset,” and watch as your imagination comes to life with stunning AI-generated artwork! ✨\n\n**📸 Transform Your Ideas!** \nGet ready for Image, you’ll be able to type **/scan_ph** along with your image description to let our AI create detailed descriptions and captivating images based on your prompts. 🔍✨\n\n\n**Get started now and see what magic awaits!**", image_keyboard)
 
     elif callback_query.data == "about":
         about_message = (
             "**ℹ️ About This Bot**\n\n"
-            "👤 **Owner:** ⏤͟͟͞͞ 🇮🇳𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's </>\n"
+            "👤 **Owner:** ⏤͟͟͞͞ 🇮🇳 @KichuXD's </>\n"
             "🤖 **Functionality:**\n"
             "- Fast and accurate answers to your questions! ⚡\n"
             "- Generate beautiful images based on your prompts! 🎨\n"
@@ -84,7 +84,7 @@ async def handle_button_click(client, callback_query):
         )
         about_keyboard = InlineKeyboardMarkup([
             [InlineKeyboardButton("🔙 ʙᴀᴄᴋ", callback_data="help"),
-             InlineKeyboardButton("🔗 ʀᴇᴘᴏ", url="https://github.com/itzAsuraa/ResponseByAi")]
+             InlineKeyboardButton("🔗 ʀᴇᴘᴏ", url="https://github.com")]
         ])
         await edit_message(client, callback_query, about_message, about_keyboard)
 
