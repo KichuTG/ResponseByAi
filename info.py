88505029d377d4e09aa2aabe7cee601eab6c270e
KichuTG <kichumon333@outlook.com>
2024-11-14 21:24:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,4 +7,4 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "7696841194:AAHMKvyXKZ6BJK8vdUDS4lhxxE6J2jb
 OWNER_ID = int(environ.get("OWNER_ID", "1905251964"))
 MONGO_URL = environ.get("MONGO_URL", "mongodb+srv://jiosaavn:jiosaavn@cluster0.ouhhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002076230596"))
-FSUB = environ.get("FSUB", True)
+FSUB = environ.get("FSUB", False)
